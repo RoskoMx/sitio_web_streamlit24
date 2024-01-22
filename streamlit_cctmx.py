566d5e6ef8ddc1fa236d2d25a9cb7b89effa0cce
RoskoMx <78753834+RoskoMx@users.noreply.github.com>
2024-01-22 11:04:17 -0600

    Update streamlit_cctmx.py

--- streamlit_cctmx.py
+++ streamlit_cctmx.py
@@ -1,1 +1,8 @@
-#
+# Contenido de la página
+import streamlit as st
+import pandas as pd
+from PIL import Image #Para insertar imágenes
+
+st.sidebar.image("", caption="Saludos desde Cannes Francia 🇫🇷")
+
+url ='https://raw.githubusercontent.com/LilianaC/Pandas/master/Fifa%2023%20Fut%20Play
